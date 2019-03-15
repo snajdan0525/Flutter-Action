a6aed28854c55207d032719b72459a3ca648a590
sNajdan <snajdan0525@gmail.com>
2019-03-15 15:06:44 +0800

    Scaffold、TabBar、底部导航

---
+++ flutter_action/lib/tabs/first.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_action/view/gridview.dart';
+
+class FirstTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new MyGridView().build(),
+    );
+  }
+}
