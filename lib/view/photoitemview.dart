728fe728931520e08d03a4f72528e39f889dd30e
sNajdan <snajdan0525@gmail.com>
2019-03-17 20:09:55 +0800

    add a grid demo which is modeled  flutter_gallery_grid_example

---
+++ lib/view/photoitemview.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_action/model/photo.dart';
+
+typedef BannerTapCallback = void Function(Photo photo);
+
+const double _kMinFlingVelocity = 800.0;
+const String _kGalleryAssetsPackage = 'flutter_gallery_assets';
+
+class GridDemoPhotoItem extends StatelessWidget {
+  GridDemoPhotoItem({Key key, @required this.photo, @required this.onBannerTap})
+      : assert(photo != null && photo.isValid),
+        assert(onBannerTap != null),
+        super(key: key);
+
+  final Photo photo;
+  final BannerTapCallback
+      onBannerTap; // User taps on the photo's header or footer.
+
+  void showPhoto(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute<void>(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text(photo.title)),
+        body: SizedBox.expand(
+          child: Hero(
+            tag: photo.tag,
+            child: GridPhotoViewer(photo: photo),
+          ),
+        ),
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget image = GestureDetector(
+        onTap: () {
+          showPhoto(context);
+        },
+        child: Hero(
+            key: Key(photo.assetName),
+            tag: photo.tag,
+            child: Image.asset(
+              photo.assetName,
+              package: photo.assetPackage,
+              fit: BoxFit.cover,
+            )));
+
+    final IconData icon = photo.isFavorite ? Icons.star : Icons.star_border;
+    return GridTile(
+      footer: GestureDetector(
+        onTap: () {
+          onBannerTap(photo);
+        },
+        child: GridTileBar(
+          backgroundColor: Colors.black45,
+          title: _GridTitleText(photo.title),
+          subtitle: _GridTitleText(photo.caption),
+          trailing: Icon(
+            icon,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      child: image,
+    );
+  }
+}
+
+class _GridTitleText extends StatelessWidget {
+  const _GridTitleText(this.text);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      alignment: Alignment.centerLeft,
+      child: Text(text),
+    );
+  }
+}
+
+
+
+class GridPhotoViewer extends StatefulWidget {
+  const GridPhotoViewer({ Key key, this.photo }) : super(key: key);
+
+  final Photo photo;
+
+  @override
+  _GridPhotoViewerState createState() => _GridPhotoViewerState();
+}
+
+class _GridPhotoViewerState extends State<GridPhotoViewer> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Offset> _flingAnimation;
+  Offset _offset = Offset.zero;
+  double _scale = 1.0;
+  Offset _normalizedOffset;
+  double _previousScale;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this)
+      ..addListener(_handleFlingAnimation);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // The maximum offset value is 0,0. If the size of this renderer's box is w,h
+  // then the minimum offset value is w - _scale * w, h - _scale * h.
+  Offset _clampOffset(Offset offset) {
+    final Size size = context.size;
+    final Offset minOffset = Offset(size.width, size.height) * (1.0 - _scale);
+    return Offset(offset.dx.clamp(minOffset.dx, 0.0), offset.dy.clamp(minOffset.dy, 0.0));
+  }
+
+  void _handleFlingAnimation() {
+    setState(() {
+      _offset = _flingAnimation.value;
+    });
+  }
+
+  void _handleOnScaleStart(ScaleStartDetails details) {
+    setState(() {
+      _previousScale = _scale;
+      _normalizedOffset = (details.focalPoint - _offset) / _scale;
+      // The fling animation stops if an input gesture starts.
+      _controller.stop();
+    });
+  }
+
+  void _handleOnScaleUpdate(ScaleUpdateDetails details) {
+    setState(() {
+      _scale = (_previousScale * details.scale).clamp(1.0, 4.0);
+      // Ensure that image location under the focal point stays in the same place despite scaling.
+      _offset = _clampOffset(details.focalPoint - _normalizedOffset * _scale);
+    });
+  }
+
+  void _handleOnScaleEnd(ScaleEndDetails details) {
+    final double magnitude = details.velocity.pixelsPerSecond.distance;
+    if (magnitude < _kMinFlingVelocity)
+      return;
+    final Offset direction = details.velocity.pixelsPerSecond / magnitude;
+    final double distance = (Offset.zero & context.size).shortestSide;
+    _flingAnimation = _controller.drive(Tween<Offset>(
+        begin: _offset,
+        end: _clampOffset(_offset + direction * distance)
+    ));
+    _controller
+      ..value = 0.0
+      ..fling(velocity: magnitude / 1000.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onScaleStart: _handleOnScaleStart,
+      onScaleUpdate: _handleOnScaleUpdate,
+      onScaleEnd: _handleOnScaleEnd,
+      child: ClipRect(
+        child: Transform(
+          transform: Matrix4.identity()
+            ..translate(_offset.dx, _offset.dy)
+            ..scale(_scale),
+          child: Image.asset(
+            widget.photo.assetName,
+            package: widget.photo.assetPackage,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
