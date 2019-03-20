85bc8e4790bcbdbb66e5e55feefd38977a5d85ef
sNajdan <snajdan0525@gmail.com>
2019-03-20 21:57:56 +0800

    add shimmer widget and ShimmerState framework code

---
+++ lib/util/shimmer.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Shimmer extends StatefulWidget {
+  @override
+  _ShimmerState createState() => new _ShimmerState();
+
+  Shimmer();
+}
+
+class _ShimmerState extends State<Shimmer> with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(Shimmer oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+  }
+}
