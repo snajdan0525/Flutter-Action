bb9e014d08e924023a64144cea3d33090e80acc4
sNajdan <snajdan0525@gmail.com>
2019-03-21 17:31:19 +0800

    add a CustomView which draws a Circle

--- lib/detailview/CircleView.dart
+++ lib/detailview/CircleView.dart
@@ -4,7 +4,6 @@ import 'package:flutter/rendering.dart';
 class CircleView extends SingleChildRenderObjectWidget {
   @override
   RenderObject createRenderObject(BuildContext context) {
-    // TODO: implement createRenderObject
     return new CircleViewRenderBox();
   }
 }
@@ -15,17 +14,24 @@ class CircleViewRenderBox extends RenderConstrainedBox {
 
   @override
   void handleEvent(PointerEvent event, HitTestEntry entry) {
-    // TODO: implement handleEvent
     super.handleEvent(event, entry);
   }
 
   @override
   void paint(PaintingContext context, Offset offset) {
-    Rect rect = Offset.zero & size;
-    context.canvas.save();
-    context.canvas.clipRRect(new RRect.fromRectXY(rect, 100.0, 100.0));
-    context.canvas.drawPaint(new Paint()..color = Colors.red);
-    context.canvas.drawPaint(new Paint()..color = Colors.white);
-    context.canvas.restore();
+    /*this code copied from paintint.dart's classs function savelayer */
+//    Rect rect = Offset.zero & size;
+//    context.canvas.save();
+//    context.canvas.clipRRect(new RRect.fromRectXY(rect, 100.0, 100.0));
+//    context.canvas.drawPaint(new Paint()..color = Colors.red);
+//    context.canvas.drawPaint(new Paint()..color = Colors.white);
+//    context.canvas.restore();
+    Paint _paint = new Paint()
+      ..color = Colors.redAccent
+      ..strokeCap = StrokeCap.round
+      ..isAntiAlias = true
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5.0;
+    context.canvas.drawCircle(Offset(100, 100), 80, _paint);
   }
 }
