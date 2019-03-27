2b89e61ebaa8bf2d1bcc3d12c6308159961057ab
sNajdan <snajdan0525@gmail.com>
2019-03-27 23:27:15 +0800

    add RefreshIndicator

--- lib/view/pull_to_refresh_listview.dart
+++ lib/view/pull_to_refresh_listview.dart
@@ -11,11 +11,31 @@ class PTRListView extends StatefulWidget {
 
 class PTRListViewState extends State<PTRListView> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
-  
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('PTRListView'),
+      ),
+      body: RefreshIndicator(
+          key: _refreshIndicatorKey,
+          onRefresh: null,
+          child: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                isThreeLine: true,
+                leading: CircleAvatar(child: Text('aaa')),
+                title: Text('This item represents'),
+                subtitle: const Text(
+                    'Even more additional list item information appears on line three.'),
+              );
+            },
+          )),
+    );
   }
 }
